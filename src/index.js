78644433c8e99378079ce435acbc53154f7f35c0
Vlad <VladDaImpaler@gmail.com>
2020-10-08 04:15:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    //Challenge 1
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(json => json.message.forEach(url => addImage(url)));
+
+    //Challenge 2
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(results =>{ 
+        breeds = Object.keys(results.message);
+        addBreed(breeds);
+
+    })
+
+});
+
+function addImage(src){
+    let img = document.createElement("img");
+    let imgContainer = document.getElementById("dog-image-container");
+    
+    img.src = src;
+    imgContainer.appendChild(img);
+}
+
+function addBreed(src)
+{
+    let breedContainer = document.getElementById("dog-breeds");
+    let node = document.createElement("li");
+    src.forEach(breed=>{
+        let description = document.createTextNode(breed);
+        node.appendChild(breed);
+        breedContainer.appendChild(node);
+    });
+
+}
