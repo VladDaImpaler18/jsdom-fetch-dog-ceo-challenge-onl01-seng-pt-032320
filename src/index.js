01a3290e27905ada7c93e8030fe5b684da6b2c21
Vlad <VladDaImpaler@gmail.com>
2020-10-08 22:10:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+const breedDropdown = document.getElementById("breed-dropdown");
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
+    const breedContainer = document.getElementById("dog-breeds");
     //Challenge 1
     fetch(imgUrl)
     .then(response => response.json())
@@ -17,6 +20,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     })
 
+
+
+document.addEventListener("click",(e) => {
+    if (e.target.nodeName==="LI"){ //Strong Island yeahhh boi
+        if(e.target.style.color===""){ e.target.style.color="red"; }
+        else if(e.target.style.color==="red"){e.target.style.color=""; }
+    }
+    else if(e.target === breedDropdown){
+        
+    }
 });
 
 function addImage(src){
@@ -24,17 +37,19 @@ function addImage(src){
     let imgContainer = document.getElementById("dog-image-container");
     
     img.src = src;
-    imgContainer.appendChild(img);
+    //imgContainer.appendChild(img);
 }
 
 function addBreed(src)
 {
-    let breedContainer = document.getElementById("dog-breeds");
-    let node = document.createElement("li");
     src.forEach(breed=>{
         let description = document.createTextNode(breed);
-        node.appendChild(breed);
+        let node = document.createElement("li");
+        node.id = breed;
+
+        node.appendChild(description);
         breedContainer.appendChild(node);
     });
 
 }
+});
